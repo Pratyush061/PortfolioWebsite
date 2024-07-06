d8ff0947e8abe4ee50c48628bda025412ddfd356
Pratyush061 <142402529+Pratyush061@users.noreply.github.com>
2024-07-06 22:24:56 +0530

    Update Website.py

--- Website.py
+++ Website.py
@@ -79,8 +79,8 @@ docker_lottie = load_lottieurl("https://assets4.lottiefiles.com/private_files/lf
 figma_lottie = load_lottieurl("https://lottie.host/5b6292ef-a82f-4367-a66a-2f130beb5ee8/03Xm3bsVnM.json")
 js_lottie = load_lottieurl("https://lottie.host/fc1ad1cd-012a-4da2-8a11-0f00da670fb9/GqPujskDlr.json")
 
-# Display the photo in the sidebar
-st.sidebar.markdown(info['Photo'], unsafe_allow_html=True)
+# # Display the photo in the sidebar
+# st.sidebar.markdown(info['Photo'], unsafe_allow_html=True)
 
 # Info section
 
