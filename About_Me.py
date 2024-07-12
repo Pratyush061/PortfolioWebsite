c5eae5f871a64603065ca0456d1d7976b2b691ba
Pratyush061 <142402529+Pratyush061@users.noreply.github.com>
2024-07-12 09:11:24 +0530

    Update About_Me.py

--- About_Me.py
+++ About_Me.py
@@ -1,7 +1,17 @@
 
 # api_key = st.secrets['GOOGLE_API_KEY']
+# Imporing the necessary Libraries
+import base64
+import threading
+from io import BytesIO
 import streamlit as st
-
+from PIL import Image
+import requests
+from streamlit_lottie import st_lottie
+from streamlit_timeline import timeline
+import streamlit.components.v1 as components
+import google.generativeai as genai
+import os
 api_key="AIzaSyC83C8rwcxdmSsR5b4bUj_EjywHCsmn9gE"
 # genai.configure(api_key=api_key)
 
@@ -10,24 +20,71 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 # Load the image
 image_path = 'images/About_Me.jpeg'
 
-text = ("Hello! I'm Pratyush Jain, a passionate"
-        " B.Tech student specializing in Information Technology at"
-        " Shri G.S. Institute Of Technology And Science, Indore, Madhya Pradesh."
-        " My journey in the world of technology and innovation has equipped me with strong skills in programming, data structures, algorithms, and several key areas of computer science.")
+
+text = (
+        "Passionate Data Scientist | "
+        "Python & ML Enthusiast |"
+        " NLP & Deep Learning "
+        "| Computer Vision"
+        "| Algorithm "
+        "| AI-driven Solutions "
+        "| Seeking to Innovate and Transform with Data."
+       )
+
 
 # Custom CSS for styling columns and containers
 container_style = """
-        <style>
-        /* Style columns */
-        [data-testid="column"] {
-            box-shadow: rgb(0 0 0 / 50%) 0px 2px 10px -1px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
-            border-radius: 100px;
-            padding: 10% 5% 5% 4.5%;
-        }
+<style>
+/* Style columns */
 
-        }
-        </style>
-        """
+
+[data-testid="column"] {
+    box-shadow: 0px 4px 12px  rgba(0, 0, 0, 5), 0px 1px 3px rgba(0, 0, 0, 8);
+    border-radius: 20px;
+    padding: 20px;
+    background-color: #a5f9f9;
+    transition: transform 0.5s;
+    # animation-name: example;
+    # animation-duration: 4s;
+}
+
+[data-testid="column"]:hover {
+    transform: scale(1.04);
+}
+
+/* Style container for the image */
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 18px;
+}
+
+.container img {
+    border-radius: 50%;
+    max-width: 90%;
+    height: auto;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+</style>
+"""
+
+
+# Profile image file
+with open("images/About_Me.jpeg", "rb") as img_file:
+    img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
+
+
+ # CSS styles file
+with open("imganimation.css") as f:
+   st.write(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+
 
 # Inject custom CSS
 st.markdown(container_style, unsafe_allow_html=True)
@@ -36,18 +93,46 @@ st.markdown(container_style, unsafe_allow_html=True)
 
 # <-----Streamlit Application for the Typewriter Effect ---->
 
-col1, col2 = st.columns(2)
+custom_col1, custom_col2 = st.columns(2)
 
-with col1:
-    st.subheader('Hi 👏🏿')
-    st.title("Pratyush J.👦🏻")
+ # CSS styles file
+with open("Typewriter.css") as f:
+    st.write(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+
+with custom_col1:
+    # Display the typewriter text
+    st.markdown(f"""
+            <div class="heading">
+                <h1>{"Hello! I am"}</h1>
+            </div>
+            """, unsafe_allow_html=True)
+    st.markdown(f"""
+        <div class="typewriter">
+            <h1>{"Pratyush J.👦🏻"}</h1>
+        </div>
+        """, unsafe_allow_html=True)
     st.write(text)
 
-with col2:
-    st.subheader(" ")
-    st.image(image_path)
+with custom_col2:
+    st.write(f"""
+    <div class="container">
+        <div class="box">
+            <div class="spin-container">
+                <div class="shape">
+                    <div class="bd">
+                        <img src="{img}" alt="Profile Image" >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
 
 # <<<<-----Streamlit Typewriter Effect------>>>>>>>>
+
+
 
 with st.container(border=True):
 
@@ -99,7 +184,8 @@ with st.container(border=True):
     Pratyush has undertaken several impactful projects. He developed a Movie Recommender System using Python and NLP techniques, achieving effective content-based recommendations. His work on Plant Disease Classification involved a robust CNN model, reaching a 97.7% accuracy on test data. In the Drug Target Prediction project, he created a deep learning model that processed molecular and protein sequence data to predict drug interactions. Additionally, he built an OpenCV AI Virtual Keyboard, incorporating real-time hand tracking and gesture recognition.
 
     Recognized for his problem-solving abilities, Pratyush is a 4-star coder on HackerRank and has received bronze medals on Kaggle for his contributions to notebooks and discussions. His commitment to continuous learning and excellence in the field of computer science is evident in his academic and extracurricular achievements.
-
+    
+    
     Pratyush is based in Ujjain, Madhya Pradesh, and can be contacted via email at pratyushjj02@gmail.com. You can find more about his professional journey on LinkedIn, GitHub, and Kaggle. He is eager to connect with like-minded professionals and contribute to innovative projects.
 
     Contact Information:
@@ -120,33 +206,22 @@ with st.container(border=True):
         prompt = "Wrong option selected"
 
 
+    # Logic to handle the button click and generate response
     if st.button("ASK", use_container_width=100):
         response = model.generate_content(prompt)
         st.write(response.text)
 
 
-# st.title("Skills")
-#
-# color = st.select_slider(
-#     "Select a color of the rainbow",
-#     options=["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
-# st.write("My favorite color is", color)
 
 with st.container(border=True):
     # Title
     st.title("Skills 🤹🏻")
 
-# # Skill sliders
-# programming = st.slider("Programming", 0, 100, 70)
-# machine_learning = st.slider("Machine Learning", 0, 100, 40)
-# computer_vision = st.slider("Computer Vision", 0, 100, 60)
-# web_development = st.slider("Web Development", 0, 100, 40)
-
 
     # Data for pie chart
     skills_data = {
         'Skills': ['Programming', 'Machine Learning', 'Computer Vision', 'Web Development'],
-        'Proficiency': [70, 50, 60, 40]
+        'Proficiency': [70, 60, 50, 40]
     }
 
     df = pd.DataFrame(skills_data)
@@ -168,15 +243,16 @@ with st.container(border=True):
 # You can get color scales from:https://plotly.com/python/builtin-colorscales/
 
 # Apply burst effect to a specific slice (adjust index for desired slice)
-    explode_values = [0, 0.2, 0, 0]   # Set burst effect for the second slice (index 1)
+    explode_values = [0, 0 ,0.15, 0]   # Set burst effect for the second slice (index 1)
     fig.update_traces(pull=explode_values)  # Apply explosion
 
 # Additional customizations (adjust as needed)
-    fig.update_traces(textposition='inside', textfont_size=14, textinfo='percent+label')
+    fig.update_traces(textposition='inside', textfont_size=15, textinfo='percent+label')
     fig.update_layout(
-        paper_bgcolor='rgba(255,100,100,0.4)',  # Set a semi-transparent background
+        paper_bgcolor='rgba(153,51,51,0.3)',  # Set a semi-transparent background
         font={'color': 'white'}  # Set white text color for better contrast
     )
 
 # Display the pie chart with burst effect
     st.plotly_chart(fig)
+
